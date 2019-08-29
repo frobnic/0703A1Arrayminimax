388fd363e0d19529c0c5c3bd70e0c6a35455df93
frank paulsen <frobnic+ita@gmail.com>
2019-08-29 10:19:20 +0200

    code inserted

--- 0703A1Arrayminimax/Program.cs
+++ 0703A1Arrayminimax/Program.cs
@@ -8,8 +8,54 @@ namespace _0703A1Arrayminimax
 {
     class Program
     {
+        static int[] ReadInArray(int NumOfElements)
+        {
+           
+            int[] zahlen = new int[NumOfElements];
+
+            for (int i=0; i < NumOfElements; i++)
+            {
+                Console.Write("Bitte Zahl {0,2} eingeben:",i+1);
+                zahlen[i] = int.Parse(Console.ReadLine());
+            }
+
+            return zahlen;
+        }
+
+        static int FindMin(int[] SearchArray)
+        {
+            int a = int.MaxValue;
+            foreach (int b in SearchArray)
+            {
+                if (b < a)
+                    a = b;
+            }
+            return a;
+        }
+
+        static int FindMax(int[] SearchArray)
+        {
+            int a = int.MinValue;
+            foreach (int b in SearchArray)
+            {
+                if (b > a)
+                    a = b;
+            }
+            return a;
+        }
+
         static void Main(string[] args)
         {
+            int[] zahlen;
+            int anzahl;
+
+            Console.Write("Wieviele Zahlen möchten Sie erfassen? (1-20)");
+            anzahl = int.Parse(Console.ReadLine());
+
+            zahlen = ReadInArray(anzahl);
+
+            Console.WriteLine("Die kleinste Zahl ist {0}", FindMin(zahlen));
+            Console.WriteLine("Die grösste  Zahl ist {0}", FindMax(zahlen));
         }
     }
 }
